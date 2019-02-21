9bc7f64035bba54dbc7dc4aacc4a989b9378efec
Syed Kamran Ahmed <skahmed@myamu.ac.in>
2019-02-21 14:28:07 +0530

    Update idiom-extractor.py

--- idiom-extractor.py
+++ idiom-extractor.py
@@ -6,14 +6,10 @@ source = requests.get('https://www.englishclub.com/ref/idiom-of-the-day.php').te
 
 soup = BeautifulSoup(source,'lxml')
 
-# content = soup.find('main', id="ec-main")
 
-# print(content.prettify())
+headline = soup.find('h1').text  
 
-
-headline = soup.find('h1').text
-
-idiom = soup.find('h2').text
+idiom = soup.find('h2').text   
 
 meaning = soup.find_all('p')[1].text
 
@@ -21,24 +17,6 @@ example = soup.find('div',class_='example')
 for_example = example.h3.text
 sentence1 = example.find_all('li')[0].text
 sentence2 = example.find_all('li')[1].text
-
-
-# print(example)
-# print(for_example)
-
-# title = soup.title.text
-def message():
-        print(headline + '\n') 
-        print(idiom+'\n')
-        print("Meaning :")
-        print(meaning + '\n')
-        print(for_example)
-        sentences = example.find_all('li')
-        for sentence in sentences:
-            print("-> "+sentence.text)
-            print()
-
-
 
 # Your Account Sid and Auth Token from twilio.com/console
 account_sid = 'Your_Account_SID'
@@ -51,4 +29,4 @@ message = client.messages.create(
                               to='whatsapp:+91XXXXXXXXXX'
                           )
 
-print(message.sid)
+print(message.sid) #optional
